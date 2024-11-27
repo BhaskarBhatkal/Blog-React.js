cdea8359662d2d056409c665640103312b2baf14
BhaskarBhatkal <b.r.moger1234@gmail.com>
2024-11-27 10:34:33 +0530

    debug

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -18,6 +18,7 @@ export default function PostForm({ post }) {
 
   const navigate = useNavigate();
   const userData = useSelector((state) => state.auth.userData);
+  console.log("Postform :: Userdata:", userData);
   const submit = async (data) => {
     if (post) {
       const file = data.image[0]
